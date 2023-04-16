a36bc809a6d4b870957d10a0acb12859ceb2cf97
Angela A. Ogares <125331582+aogares@users.noreply.github.com>
2023-04-17 01:52:00 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
+const { initializeApp } = require("firebase/app");
+const { initializeAppCheck, ReCaptchaV3Provider } = require("firebase/app-check");
+
+const app = initializeApp({
+  // Your firebase configuration object
+});
+
+// Pass your reCAPTCHA v3 site key (public key) to activate(). Make sure this
+// key is the counterpart to the secret key you set in the Firebase console.
+const appCheck = initializeAppCheck(app, {
+  provider: new ReCaptchaV3Provider('abcdefghijklmnopqrstuvwxy-1234567890abcd'),
+
+  // Optional argument. If true, the SDK automatically refreshes App Check
+  // tokens as needed.
+  isTokenAutoRefreshEnabled: true
+});
+
 let form = document.getElementById("form");
 let textInput = document.getElementById("textInput");
 let dateInput = document.getElementById("dateInput");
