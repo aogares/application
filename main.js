a931bb48315552edbaee18812b6b359c817ae4eb
Angela A. Ogares <125331582+aogares@users.noreply.github.com>
2023-04-17 02:11:16 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,25 +1,3 @@
-// Import the functions you need from the SDKs you need
-    import { initializeApp } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-app.js";
-    import { getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-database.js";
-
-    // TODO: Add SDKs for Firebase products that you want to use
-    // https://firebase.google.com/docs/web/setup#available-libraries
-  
-    // Your web app's Firebase configuration
-    const firebaseConfig = {
-    apiKey: "AIzaSyDpnwboJUwHtx0-L2z1QYvfuxTQbSuWJRk",
-    authDomain: "crud-d7cb9.firebaseapp.com",
-    databaseURL: "https://crud-d7cb9-default-rtdb.firebaseio.com",
-    projectId: "crud-d7cb9",
-    storageBucket: "crud-d7cb9.appspot.com",
-    messagingSenderId: "80177244965",
-    appId: "1:80177244965:web:c8db16de1da90abb5b88cc"
-  };
-  
-    // Initialize Firebase
-    const app = initializeApp(firebaseConfig);
-    const database = getDatabase(app);
-
 const app = initializeApp({
   // Your firebase configuration object
 });
